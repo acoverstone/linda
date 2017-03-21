44163b33386720f293a22633a23d9e2c4fb42735
Brandon Duong <Brandon Duong>
2017-03-21 19:57:33 -0400

    inheritance for commands

---
+++ commands/command.py
@@ -0,0 +1,13 @@
+import abc
+
+class Command(object):
+    def decode(self,input):    #method to check the speech input with our list of words
+        for instruction in self.INSTRUCTIONS:  #loops through all our instructions
+            if instruction in input:    #checks if it needs to execute this command
+                self.execute()       #executes the command
+
+    @abc.abstractmethod     
+    def execute(self):
+        #abstract function that holds all the logic for
+        #    performing each command
+        pass

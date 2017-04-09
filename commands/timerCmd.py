821252395245692eca72b66ddacbe6c652745e6e
acoverstone <acoverstone@ufl.edu>
2017-04-09 15:47:06 -0500

    Just commiting to test Joe's stuff

--- commands/timerCmd.py
+++ commands/timerCmd.py
@@ -3,14 +3,30 @@ import threading
 import speech
 import sys
 
-class TimerCmd(Command):             #command for telling jokes
+class TimerCmd(Command):             #command for starting a timer
     INSTRUCTIONS = ['timer'] #list of keywords
-    def execute(self):              #filled in abstract execute method
-        t = threading.Timer(5.0, hello)
-        t.start() # after 30 seconds, "hello, world" will be printed
+    def execute(self):
+        speech.speak("How long do you wish to set the timer for?")
+        # Input must be in the form of ____ hour, ___ minute, _____ seconds
+        response = speech.takeInput()
+
+        understood = False
+
+        while(not understood):
+            if("hours" in response or "hour" in response or "minutes" in response or "minute" in response or "seconds" in response or "second" in response):
+                speech.speak("Timer starting");
+                understood = True
+            else:
+                 speech.speak("I'm sorry I did not understand that. Please answer by saying blank hours blank minutes or blank seconds.");
+            
+        
+        # t = threading.Timer(5.0, hello)
+        # t.start() 
 
 def hello():
     speech.speak("beep beep beep beep")
     print "hello, world"
 
-    
+def parseTime(response):
+    if "second" in response or "seconds" in response:
+        

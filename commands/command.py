cbfc067977cf72b41475667473909e4927f3a691
Brandon Duong <Brandon Duong>
2017-04-09 19:08:00 -0400

    fixing Screens not defined in commands

--- commands/command.py
+++ commands/command.py
@@ -1,7 +1,8 @@
 import abc
 
 class Command(object):
-    def decode(self,input,Screens):    #method to check the speech input with our list of words
+    def decode(self,input,args):    #method to check the speech input with our list of words
+        Screens = args
         for instruction in self.INSTRUCTIONS:  #loops through all our instructions
             if instruction in input:    #checks if it needs to execute this command
                 self.execute()       #executes the command

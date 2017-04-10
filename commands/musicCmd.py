5a4fad226868ca98ea3cefdfd3569d64588b210b
Brandon Duong <Brandon Duong>
2017-04-09 21:20:40 -0400

    fixed indentation

--- commands/musicCmd.py
+++ commands/musicCmd.py
@@ -7,23 +7,23 @@ class MusicCmd(Command):
     songs = ['better together']
 	INSTRUCTIONS = ['music', 'song']
 	def execute(self,Screens):
-        Screens.show_frame("MusicScreen")
-		speech.speak("What song would you like to play?")
-        found = False
+            Screens.show_frame("MusicScreen")
+    		speech.speak("What song would you like to play?")
+            found = False
 
-        while not found:
-            response = speech.takeInput()
+            while not found:
+                response = speech.takeInput()
 
-            if "quit" in response:
-                break
+                if "quit" in response:
+                    break
 
-            for song in self.songs:
-                if song in response:
-                    found = True
+                for song in self.songs:
+                    if song in response:
+                        found = True
 
-        	if(not found):
-                    speech.speak("This song does not exist in the library.")
-                    self.execute(self,Screens)
-                    return
-            if("better together" in response):
-                        os.system("aplay resources/music/better_together.wav")
+            	if(not found):
+                        speech.speak("This song does not exist in the library.")
+                        self.execute(self,Screens)
+                        return
+                if("better together" in response):
+                            os.system("aplay resources/music/better_together.wav")

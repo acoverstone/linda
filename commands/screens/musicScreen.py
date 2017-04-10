5ef892cd2fba200d94b74d873791e951ae5aa22b
Brandon Duong <Brandon Duong>
2017-04-09 21:09:30 -0400

    adding music gif

---
+++ commands/screens/musicScreen.py
@@ -0,0 +1,15 @@
+import Tkinter as tk
+
+class WeatherScreen(tk.Frame):
+    def __init__(self, parent, controller):
+        global label
+        height = 2000
+        width = 2000
+        tk.Frame.__init__(self, parent,width=width,height=height,bg="black")
+        self.controller = controller
+        label = tk.Label(self, text="Weather Forecast", fg="white",bg="black",font=("Helvetica", 30))
+        label.place(relx=0.5,rely=0.2,anchor='center')
+        photo = tk.PhotoImage(file = '../../resources/images/music.gif')
+        gif = tk.Label(self ,image = photo)
+        gif.image = photo
+        gif.grid(row=0, column=0)

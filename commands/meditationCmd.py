0094adb7b83c9e9f17d4c1d1f99e35fc38183122
Joseph Mullen <jmullen@Josephs-MacBook-Pro.local>
2017-04-11 15:58:57 -0400

    random meditations

--- commands/meditationCmd.py
+++ commands/meditationCmd.py
@@ -1,24 +1,25 @@
 from command import Command
 import speech
 import os
+import random
 
 
 class MeditationCmd(Command):
-    meditations = ['one']
+    meditations = [0]
     INSTRUCTIONS = ['meditate', 'meditation']
     def execute(self,Screens):
         found = False
         while not found:
-            speech.speeak("What number meditation do you want to play?")
-            response = speech.takeInput()
-            if "quit" in response:
-                break
+            int numOfMeditations = len(meditations)
+            int playing = random.randint(0, numOfMeditations)
+
             for meditation in self.meditations:
-                if meditation in response:
+                if meditation is playing:
                     found = True
+                    if(playing = 0):
+                        os.system("aplay resources/meditations/one.wav")
             if(not found):
-                speech.speak("This meditation does not exist in the library.")
+                speech.speak("There are no meditations in your library")
                 self.execute(Screens)
                 return
-            if("one" in response):
-                os.system("aplay resources/meditations/one.wav")
+            

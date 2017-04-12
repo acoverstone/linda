30600dbaee18d85d4d5627d63cb7ef0d65ddbcc1
Brandon Duong <Brandon Duong>
2017-04-12 04:36:30 -0400

    weather screen

--- commands/weatherCmd.py
+++ commands/weatherCmd.py
@@ -53,11 +53,11 @@ class WeatherCmd(Command):             #command for telling jokes
         day_of_the_week = dtu.strftime("%A")
 
         dtu = str(dtu)
-        day2 = ""
-        day2 += ('\nday 2\n')
-        day2 += ((dtu[5:7] + '/' + dtu[8:10] + '/' + dtu[0:4] + '\n'))
-        day2 += (day_of_the_week + '\n')
-        day2 += (string.capwords(day1._status) + '\n')
+        day2String = ""
+        day2String += ('\nday 2\n')
+        day2String += ((dtu[5:7] + '/' + dtu[8:10] + '/' + dtu[0:4] + '\n'))
+        day2String += (day_of_the_week + '\n')
+        day2String += (string.capwords(day1._status) + '\n')
 
         day2tempmin = str(round(day2._temperature['min']*9/5-459.67,2))
         day2tempmax = str(round(day2._temperature['max']*9/5-459.67,2))
@@ -65,13 +65,13 @@ class WeatherCmd(Command):             #command for telling jokes
         day2tempday = str(round(day2._temperature['day']*9/5-459.67,2))
         day2tempeve = str(round(day2._temperature['eve']*9/5-459.67,2))
         day2tempnight = str(round(day2._temperature['night']*9/5-459.67,2))
-        day2 += ('min ' + day2tempmin + '\n')
-        day2 += ('max ' + day2tempmax + '\n')
-        day2 += ('morn ' + day2tempmorn + '\n')
-        day2 += ('day ' + day2tempday + '\n')
-        day2 += ('eve ' + day2tempeve + '\n')
-        day2 += ('night ' + day2tempnight + '\n')
-        frame.day2(day2)
+        day2String += ('min ' + day2tempmin + '\n')
+        day2String += ('max ' + day2tempmax + '\n')
+        day2String += ('morn ' + day2tempmorn + '\n')
+        day2String += ('day ' + day2tempday + '\n')
+        day2String += ('eve ' + day2tempeve + '\n')
+        day2String += ('night ' + day2tempnight + '\n')
+        frame.day2(day2String)
 
         #day3
         day3 = fd._forecast._weathers[2]
@@ -80,10 +80,11 @@ class WeatherCmd(Command):             #command for telling jokes
         day_of_the_week = dtu.strftime("%A")
 
         dtu = str(dtu)
-        day3 += ('\nday 3' + '\n')
-        day3 += ((dtu[5:7] + '/' + dtu[8:10] + '/' + dtu[0:4] + '\n'))
-        day3 += (day_of_the_week + '\n')
-        day3 += (string.capwords(day3._status) + '\n')
+        day3String = ""
+        day3String += ('\nday 3' + '\n')
+        day3String += ((dtu[5:7] + '/' + dtu[8:10] + '/' + dtu[0:4] + '\n'))
+        day3String += (day_of_the_week + '\n')
+        day3String += (string.capwords(day3._status) + '\n')
 
         day3tempmin = str(round(day3._temperature['min']*9/5-459.67,2))
         day3tempmax = str(round(day3._temperature['max']*9/5-459.67,2))
@@ -91,13 +92,13 @@ class WeatherCmd(Command):             #command for telling jokes
         day3tempday = str(round(day3._temperature['day']*9/5-459.67,2))
         day3tempeve = str(round(day3._temperature['eve']*9/5-459.67,2))
         day3tempnight = str(round(day3._temperature['night']*9/5-459.67,2))
-        day3 += ('min ' + day3tempmin + '\n')
-        day3 += ('max ' + day3tempmax + '\n')
-        day3 += ('morn ' + day3tempmorn + '\n')
-        day3 += ('day ' + day3tempday + '\n')
-        day3 += ('eve ' + day3tempeve + '\n')
-        day3 += ('night ' + day3tempnight + '\n')
-        frame.day3(day3)
+        day3String += ('min ' + day3tempmin + '\n')
+        day3String += ('max ' + day3tempmax + '\n')
+        day3String += ('morn ' + day3tempmorn + '\n')
+        day3String += ('day ' + day3tempday + '\n')
+        day3String += ('eve ' + day3tempeve + '\n')
+        day3String += ('night ' + day3tempnight + '\n')
+        frame.day3(day3String)
 
         #day4
         day4 = fd._forecast._weathers[3]
@@ -106,10 +107,11 @@ class WeatherCmd(Command):             #command for telling jokes
         day_of_the_week = dtu.strftime("%A")
 
         dtu = str(dtu)
-        day4 += ('\nday 4' + '\n')
-        day4 += ((dtu[5:7] + '/' + dtu[8:10] + '/' + dtu[0:4] + '\n'))
-        day4 += (day_of_the_week + '\n')
-        day4 += (string.capwords(day4._status) + '\n')
+        day4String = ""
+        day4String += ('\nday 4' + '\n')
+        day4String += ((dtu[5:7] + '/' + dtu[8:10] + '/' + dtu[0:4] + '\n'))
+        day4String += (day_of_the_week + '\n')
+        day4String += (string.capwords(day4._status) + '\n')
 
         day4tempmin = str(round(day4._temperature['min']*9/5-459.67,2))
         day4tempmax = str(round(day4._temperature['max']*9/5-459.67,2))
@@ -117,13 +119,13 @@ class WeatherCmd(Command):             #command for telling jokes
         day4tempday = str(round(day4._temperature['day']*9/5-459.67,2))
         day4tempeve = str(round(day4._temperature['eve']*9/5-459.67,2))
         day4tempnight = str(round(day4._temperature['night']*9/5-459.67,2))
-        day4 += ('min ' + day4tempmin + '\n')
-        day4 += ('max ' + day4tempmax + '\n')
-        day4 += ('morn ' + day4tempmorn + '\n')
-        day4 += ('day ' + day4tempday + '\n')
-        day4 += ('eve ' + day4tempeve + '\n')
-        day4 += ('night ' + day4tempnight + '\n')
-        frame.day4(day4)
+        day4String += ('min ' + day4tempmin + '\n')
+        day4String += ('max ' + day4tempmax + '\n')
+        day4String += ('morn ' + day4tempmorn + '\n')
+        day4String += ('day ' + day4tempday + '\n')
+        day4String += ('eve ' + day4tempeve + '\n')
+        day4String += ('night ' + day4tempnight + '\n')
+        frame.day4(day4String)
 
         #day5
         day5 = fd._forecast._weathers[4]
@@ -132,10 +134,11 @@ class WeatherCmd(Command):             #command for telling jokes
         day_of_the_week = dtu.strftime("%A")
 
         dtu = str(dtu)
-        day5 += ('\nday 5' + '\n')
-        day5 += ((dtu[5:7] + '/' + dtu[8:10] + '/' + dtu[0:4]) + '\n')
-        day5 += (day_of_the_week + '\n')
-        day5 += (string.capwords(day5._status) + '\n')
+        day5String = ""
+        day5String += ('\nday 5' + '\n')
+        day5String += ((dtu[5:7] + '/' + dtu[8:10] + '/' + dtu[0:4]) + '\n')
+        day5String += (day_of_the_week + '\n')
+        day5String += (string.capwords(day5._status) + '\n')
 
         day5tempmin = str(round(day5._temperature['min']*9/5-459.67,2))
         day5tempmax = str(round(day5._temperature['max']*9/5-459.67,2))
@@ -143,13 +146,13 @@ class WeatherCmd(Command):             #command for telling jokes
         day5tempday = str(round(day5._temperature['day']*9/5-459.67,2))
         day5tempeve = str(round(day5._temperature['eve']*9/5-459.67,2))
         day5tempnight = str(round(day5._temperature['night']*9/5-459.67,2))
-        day5 += ('min ' + day5tempmin + '\n')
-        day5 += ('max ' + day5tempmax + '\n')
-        day5 += ('morn ' + day5tempmorn + '\n')
-        day5 += ('day ' + day5tempday + '\n')
-        day5 += ('eve ' + day5tempeve + '\n')
-        day5 += ('night ' + day5tempnight + '\n')
-        frame.day5(day5)
+        day5String += ('min ' + day5tempmin + '\n')
+        day5String += ('max ' + day5tempmax + '\n')
+        day5String += ('morn ' + day5tempmorn + '\n')
+        day5String += ('day ' + day5tempday + '\n')
+        day5String += ('eve ' + day5tempeve + '\n')
+        day5String += ('night ' + day5tempnight + '\n')
+        frame.day5(day5String)
 
 ##weatherCmd = WeatherCmd()
 ##weatherCmd.decode("weather")

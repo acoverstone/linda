ebde13e2d24d3478d60a61cf48426ec1de6a976f
Brandon Duong <Brandon Duong>
2017-04-12 04:06:34 -0400

    fixed var name and cleared screen after joke is done

--- commands/jokeCmd.py
+++ commands/jokeCmd.py
@@ -8,23 +8,38 @@ class JokeCmd(Command):             #command for telling jokes
     def execute(self,Screens):              #filled in abstract execute method
         Screens.show_frame("JokeScreen")
         frame = Screens.getFrame()
+
+        knock = tk.Label(self, text="Knock Knock", fg="white",bg="black",font=("Helvetica", 30))
+        knock.place(relx=0.5,rely=0.15,anchor='center')
+        Screens.update()
+
         speech.speak("Knock, Knock")
         response = speech.takeInput()
         if("there" not in response):
             speech.speak("the correct response is, who's there.")
-            frame.clearScreen()
+            knock.destroy()
             self.execute(Screens)
             return
+
+        response1 = tk.Label(self, text="Who", fg="white",bg="black",font=("Helvetica", 30))
+        response1.place(relx=0.5,rely=0.2,anchor='center')
+
         speech.speak("who");
         frame.response1()
         Screens.update()
         response = speech.takeInput();
         if("who" not in response):
             speech.speak("the correct response is, who who.")
+            knock.destroy()
+            response1.destroy()
             frame.clearScreen()
             self.execute(Screens)
             return
         frame.response2()
+
+        response2 = tk.Label(self, text="Is there an owl in here? LOL", fg="white",bg="black",font=("Helvetica", 30))
+        response2.place(relx=0.5,rely=0.3,anchor='center')
+
         Screens.update()
         speech.speak("Is there an owl in here? LOL")
         sleep(5)

5fb8a9d8eb1f73775b2a38f99ecd77addfad5e59
Brandon Duong <Brandon Duong>
2017-04-12 03:56:36 -0400

    fixed var name and cleared screen after joke is done

--- commands/screens/jokeScreen.py
+++ commands/screens/jokeScreen.py
@@ -4,28 +4,31 @@ import Tkinter as tk
 class JokeScreen(tk.Frame):
     def __init__(self, parent, controller):
         global label
-        global knockl
-        global response1
-        global response2
         height = 2000
         width = 2000
         tk.Frame.__init__(self, parent,width=width,height=height,bg="black")
         self.controller = controller
         label = tk.Label(self, text="Knock, Knock", fg="white",bg="black",font=("Helvetica", 30))
-        knockl = tk.Label(self, text="Knock Knock", fg="white",bg="black",font=("Helvetica", 30))
-        response1 = tk.Label(self, text="Who", fg="white",bg="black",font=("Helvetica", 30))
-        response2 = tk.Label(self, text="Is there an owl in here? LOL", fg="white",bg="black",font=("Helvetica", 30))
         label.place(relx=0.5,rely=0.1,anchor='center')
 
 
     def knock(self):
+        global knockl
         label.destroy()
+        knockl = tk.Label(self, text="Knock Knock", fg="white",bg="black",font=("Helvetica", 30))
         knockl.place(relx=0.5,rely=0.15,anchor='center')
     def response1(self):
+        global response1
+        response1 = tk.Label(self, text="Who", fg="white",bg="black",font=("Helvetica", 30))
         response1.place(relx=0.5,rely=0.2,anchor='center')
     def response2(self):
+        global response2
+        response2 = tk.Label(self, text="Is there an owl in here? LOL", fg="white",bg="black",font=("Helvetica", 30))
         response2.place(relx=0.5,rely=0.3,anchor='center')
     def clearScreen(self):
+        global knockl
+        global response1
+        global response2
         knockl.destroy()
         response1.destroy()
         response2.destroy()
